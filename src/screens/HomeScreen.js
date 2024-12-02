07fa315f3b65ec0e04a0117527b009831f96b06d
root <quanaimadi@gmail.com>
2024-12-03 02:35:24 +0500

    Accordion.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,13 +14,13 @@ function HomeScreen() {
         </div>;
     }
     return (
-        <div className="App flex size-full flex-col items-center justify-center">
+        <div className="App flex flex-col items-center justify-center">
             <Frame>
                 <Header/>
                 <h1 className="text-white font-bold font-sans text-4xl"> AITU Complaints</h1>
-                <h1 className="font-medium text-center text-[#555] m-10 font-montserrat "> Я ваш комплаенс-бот
-                    AITU.
-                    Моя цель — помочь вам решить проблемы и ответить на вопросы. </h1>
+                <p className="text-center size-full pr-5 pl-5 text-[#555] m-10 font-montserrat text-sm-center"> Я ваш
+                    комплаенс-бот AITU.
+                    Моя цель — помочь вам решить проблемы и ответить на вопросы. </p>
                 <div className="flex justify-between items-center mb-3.5 gap-4">
                     <button
                         className="p-4 pl-6 pr-6 text-white bg-transparent border-2 text-xl rounded-xl hover:bg-white hover:text-black font-montserrat"
