0079d9d1b3ec07726b65204a81f16c92f0051a5d
root <quanaimadi@gmail.com>
2024-12-09 23:55:52 +0500

    Accordion.js

--- src/screens/AccordionScreen.js
+++ src/screens/AccordionScreen.js
@@ -2,6 +2,6 @@ import {Choices} from "../components/Accordion";
 
 export const AccordionScreen = () => <div
     className="accordion-screen size-full flex justify-center w-full h-full">
-    <h1 className="text-white mt-[10%]">Выберите категорию</h1>
+    <h2 className="text-white mt-[10%]">Выберите категорию</h2>
     <Choices/>
 </div>
